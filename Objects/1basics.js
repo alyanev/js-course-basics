5a39a5528ba4fc572a109d389217ac69d4659036
Aleksandar Yanev <aleksandar.yanev@protonmail.com>
2025-04-28 13:33:25 +0300

    Add Objects folder (new section of the course)

---
+++ Objects/1basics.js
@@ -0,0 +1,15 @@
+// Declaring an object
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw(); // Method
