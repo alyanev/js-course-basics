feacaf1010c39c6cf2b1cb7768b8502ba06c5d56
Aleksandar Yanev <aleksandar.yanev@protonmail.com>
2025-04-30 23:58:27 +0300

    Add comments to exerice 17

--- Arrays/17Exercise-ArrayFromRange.js
+++ Arrays/17Exercise-ArrayFromRange.js
@@ -1,3 +1,4 @@
+// Exercise: Create a function called arrayFromRange that takes two arguments, min and max, and returns an array containing all the numbers in that range, including min and max.
 const numbers = arrayFromRange(1, 5);
 
 console.log(numbers);
@@ -8,3 +9,7 @@ function arrayFromRange(min, max) {
         array.push(element)
     return array;
 }
+
+// The function should work for both positive and negative ranges.
+const numbers2 = arrayFromRange(-10, -4);
+console.log(numbers2);
